3adcccd0783f42eea7ce08e38fb477d28680c70f
Nitin04kumar <04nitinkumar@gmail.com>
2025-07-28 02:53:38 +0530

    Upload

--- src/Components/Clinical_record_input.jsx
+++ src/Components/Clinical_record_input.jsx
@@ -1,5 +1,12 @@
 import styles from "./Clinical_record.module.css";
 
 export default function Clinical_record_input() {
-  return <></>;
+  return (
+    <div className={styles.modalOverlay}>
+      <div className={styles.modalContent}>
+        <h3>New Record</h3>
+        <form></form>
+      </div>
+    </div>
+  );
 }
